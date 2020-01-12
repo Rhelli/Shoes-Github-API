2ce7713d8bffb2c8c67170a37056dd061cdcef2b
Rory Hellier <rory.hellier@googlemail.com>
2020-01-11 19:32:56 -0500

    Updated shoes gui layout, buttons moved to top. Icon needs adding

--- lib/git_shoes.rb
+++ lib/git_shoes.rb
@@ -1,27 +1,30 @@
-Shoes.app(width:1280, height: 720) {
-  background "#24292E"
-  stack {
-    background "#302C2C".."#433D3D"
-    banner "GITSHOES", width: 100, stroke: white
-  }
-
-  stack(margin: 60) {
-    para "Search Github"
+Shoes.app(width:1280, height: 720) do
+  background "#FFF"
+  border "24292E", strokewidth: 8
+  stack do
     flow do
+      background "#24292E"
+      image "static/assets/img/icon_64.png"
+      subtitle "GitHome", width: 100, stroke: white
+      para "Search", stroke: white
       edit_line
       button "Go"
     end
-  }
-  
-  
-  stack(margin: 10) {
-    button("GitHub Homepage")
-    button("Profile")
-    button("Notifications")
-    button("Current Repositories")
-    button("New Repository")
-    button("Pull Requests")
-    button("Isssues")
-    button("Github Search")
-  }
-}
+  end
+
+  stack do
+    width: 99
+    border "24292E", strokewidth: 8
+    flow do
+      background "#dcdcdc"
+      button("GitHub Homepage")
+      button("Profile")
+      button("Notifications")
+      button("Current Repositories")
+      button("New Repository")
+      button("Pull Requests")
+      button("Isssues")
+      button("Github Search")
+    end
+  end
+end
