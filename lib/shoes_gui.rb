0c29f7aaf72f7267da486aa7bc9684a375fe3db0
Rory Hellier <rory.hellier@googlemail.com>
2020-01-13 12:09:59 -0500

    Updated gui and added info popup screen

--- lib/shoes_gui.rb
+++ lib/shoes_gui.rb
@@ -6,8 +6,11 @@ Shoes.app(width: 1280, height: 720) do
   background "#FAFBFC"
   border "#525457".."#24292E", strokewidth: 12
 
+  # TOP LEVEL - ICON, TITLE & SEARCH --------------------------------------------------------------------------------/
   stack(height: 60) do
     background "#24292E".."#2F3438"
+    
+    # LOGO --------------------------------------------------------------------------------/
     flow do
       stack width: "5%" do
         flow(height: 60) do
@@ -15,23 +18,84 @@ Shoes.app(width: 1280, height: 720) do
         end
       end
 
-      stack width: "65%" do
+      # TITLE --------------------------------------------------------------------------------/
+      stack width: "60%" do
         flow(width: "63%", height: 60, displace_top: 17, displace_left: 10) do
           title "GitShoes", width: 100, stroke: "#FCFCFC", font: "OpenSans normal 26"
           inscription "v1.0", stroke: white, font: "OpenSans normal 10"
         end
       end
 
-      stack width: "30%" do
-        flow(height: 60, displace_top: 24, displace_left: 45) do
-          para "Search", stroke: "#C8CACB", font: "OpenSans normal 11", displace_top: 2
+      # SEARCH & INFORMATION BUTTON  --------------------------------------------------------------------------------/
+      stack width: "35%" do
+        flow(height: 60, displace_top: 24, displace_left: 19) do
+          para "Search Github:", stroke: "#C8CACB", font: "OpenSans normal 11", displace_top: 2
           edit_line margin_left: 10, margin_right: 5
           button icon: "#{DIR}/static/search.png", width: 33, height: 24
+          button icon: "#{DIR}/static/gitcons/information.png", width: 33, height: 24, displace_left: 25 do
+            
+            # INFORMATION POP UP  --------------------------------------------------------------------------------/
+            window(height: 500, width: 900, resizable: false) do
+              background "#FAFBFC"
+              border "#525457".."#24292E", strokewidth: 12
+
+              # INFORMATION TITLE  --------------------------------------------------------------------------------/
+              stack width: "100%" do
+                background "#24292E".."#2F3438"
+                flow(width: "63%", height: 60, displace_top: 17, displace_left: 10) do
+                  title "GitShoes", width: 100, stroke: "#FCFCFC", font: "OpenSans normal 26"
+                  inscription "v1.0", stroke: white, font: "OpenSans normal 10"
+                end
+              end
+
+              # INFO TEXT  --------------------------------------------------------------------------------/
+              stack(width: "100%") do
+                background "#2F3438".."#525457"
+                subtitle "Gitshoes is an open source, desktop based Github tool for programmers, built from Ruby, Shoes! 3 GUI & Github's API.", font: 'OpenSans.ttf normal 12', displace_left: 8, stroke: white
+              end
+              stack(width: "100%", displace_left: 8) do
+                para "Gitshoes enables programmers to easily view the most frequently used pages on Github, natively, from their desktop. Gitshoes includes the following functionality", font: "OpenSans 12"
+              end
+              stack(width: "100%", displace_left: 8) do
+                para " - View Your Dashboard", font: "Poppins normal 11"
+              end
+              stack(width: "100%", displace_left: 8) do
+                para " - View Your Profile ", font: "Poppins normal 11"
+              end
+              stack(width: "100%", displace_left: 8) do
+                para " - View Your Notifications", font: "Poppins normal 11"
+              end
+              stack(width: "100%", displace_left: 8) do
+                para " - View Your Existing Repositories", font: "Poppins normal 11"
+              end
+              stack(width: "100%", displace_left: 8) do
+                para " - Create New Repositories", font: "Poppins normal 11"
+              end
+              stack(width: "100%", displace_left: 8) do
+                para " - View Pull Requests", font: "Poppins normal 11"
+              end
+              stack(width: "100%", displace_left: 8) do
+                para " - View Issues", font: "Poppins normal 11"
+              end
+              stack(width: "100%", displace_left: 8) do
+                para " - Sign In/Sign Out Of Your Github Accounts", font: "Poppins normal 11"
+              end
+              stack(width: "100%", displace_left: 8) do
+                para "", font: "Poppins normal 11"
+              end
+              stack(width: "100%", displace_left: 8) do
+                inscription "Gitshoes was created by Rory Hellier, 2020.", "Poppins normal 10"
+              end
+
+
+            end
+          end
         end
       end
     end
   end
 
+  # NAVIGATION BUTTONS  --------------------------------------------------------------------------------/
   stack(height: 60) do
     background "#2F3438".."#525457"
     flow(width: 1280, displace_top: 18, displace_left: 8) do
